37a7aa898a821e620715c50f8c5356db381f2a61
HironmoyRay <hpray35@gmail.com>
2025-05-19 17:57:32 +0600

    condition

--- C/final/Magical_Tree.c
+++ C/final/Magical_Tree.c
@@ -1,8 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
 int main()
 {
-    1 - 5+1 = 6 -> 11
-    3 - 5+2 = 7 -> 13
-    5 - 5+3 = 8 -> 15
+    int n;
+    scanf("%d", &n);
+    int rows = 6 + n / 2;
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= rows - i; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= n; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
     return 0;
 }
