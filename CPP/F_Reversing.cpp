c54a911f33e08fc5e9c60af2c13b7564d10830a3
hironmoy-ray <hpray35@gmail.com>
2025-05-29 23:08:13 +0600

    CPP

--- CPP/F_Reversing.cpp
+++ CPP/F_Reversing.cpp
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-// #include <bits/stdc++.h>
-// using namespace std;
-// int main(){
-//     int n;
-//     cin>>n;
-//     int arr[n];
-//     for (int i = 0, j=(n-1); i < n/2, j>=0; i++,j--)
-//     {
-//         if(i==j){
-//             break;
-//         }
-//         swap(arr[i], arr[j]);
-//     }
-//     for (int i = 0; i < n; i++)
-//     {
-//         cout<<arr[i]<<" ";
-//     }
-    
-    
-    
-//     return 0;
-// }
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -43,17 +19,3 @@ int main() {
 
     return 0;
 }
-=======
-#include <iostream>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    int arr[3];
-    for (int i = 0; i < n; i++)
-    {
-        cin>>arr[i];
-    }
-    
-}
->>>>>>> a92a9fc1f9891b739619ea798ec2af0b3ed9f0f5
